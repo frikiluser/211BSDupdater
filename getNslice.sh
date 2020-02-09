b37538ce9a655f68e95cbca97ecfdf2d11b921df
Antoni Villalonga <lfs@friki.cat>
2020-02-10 00:11:46 +0100

    Fix getNslice.sh: Acomodate to >100

--- getNslice.sh
+++ getNslice.sh
@@ -23,11 +23,25 @@ fi
 ## Slice
 
 case "$PATCH" in
+    1)
+        cat ./patches/$PATCH > /tmp/$PATCH.artifact
+        ;;
+    100)
+        sed -e '1,/another issue/ d' < ./patches/$PATCH > /tmp/$PATCH.artifact
+        ;;
+    184)
+        sed -e '1,/====snip/ d' < ./patches/$PATCH > /tmp/$PATCH.artifact
+        ;;
     448)
         sed -e '1,/pdp is alive.*/ d' < ./patches/$PATCH > /tmp/$PATCH.artifact
         ;;
     *)
-        sed -e '1,/.*[-|=].*[c|C]ut here.*[-|=].*/ d' < ./patches/$PATCH > /tmp/$PATCH.artifact
+        sed -e '1,/.*[-|=].*[c|C]ut\ *here/ d' < ./patches/$PATCH > /tmp/$PATCH.artifact
+        grep -c '' /tmp/$PATCH.artifact > /dev/null
+        if [ "$?" != "0" ]
+        then
+            sed -e '1,/^======/ d' < ./patches/$PATCH > /tmp/$PATCH.artifact
+        fi
         ;;
 esac
 
