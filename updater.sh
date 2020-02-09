60a9c8ff792196a395e35a2317d084926e73c458
Antoni Villalonga <lfs@friki.cat>
2020-02-09 11:32:46 +0100

    Add updater.sh

---
+++ updater.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CURRENT=`grep "Current Patch Level:" /VERSION | head -1 | awk '{print $4}'`
+PATCH=`echo $CURRENT+1|bc`
+export $PATCH
+
+while (test -e ./updates/$PATCH)
+do
+    echo Updating to 2.11 Patch Level: $PATCH
+    sh ./updates/$PATCH
+    CURRENT=`grep "Current Patch Level:" /VERSION | head -1 | awk '{print $4}'`
+    PATCH=`echo $CURRENT+1|bc`
+done
